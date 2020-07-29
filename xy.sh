61a8f2016dbfe908ed6524cef8af5561ee6b9977
jadon-paulse <jadonpaulse@gmail.com>
2020-07-29 15:15:48 +0200

    added files

---
+++ xy.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+#Jack Stevenson
+
+# take 2 inputs x and y
+
+#check if x is less than y - output "X is less than Y"
+read -p "Enter X: " x;
+read -p "Enter Y: " y;
+
+echo $x;
+echo $y;
+
+# else output "Y is less than X"
+
+
+if [ $x -lt $y ];
+    then echo "X is less than Y";
+    else echo "Y is less than X";
+fi;
